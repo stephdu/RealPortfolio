7b8a201cae84bae91df6190094d15c6fe92c4373
Stephany Du <Stephany@My-MacBook-Air.local>
2018-01-29 21:54:33 -0800

    buttons

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-const express = require('express')
-const app = express()
+//it requires express for the app the run
+const express = require('express');
+// creating a new express app
+const app = express();
+//where it will be running
+const PORT = 3000;
+//db=database;directory - db, file called contacts
+//const CONTACTS = require('./db/contacts');
+const bodyParser = require('body-parser');
 
-app.get('/', (req,res) => {
+app.use(express.static(__dirname));
+//defining what's called a route; what we see after the URL when we go to a page; giving it a specific name = end point
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
-}
-
-app.get('/contact',(req,res)) => {
-  res.sendFile(__dirname + '/contact.html')
-}
-
-app.get('/about me',(req,res)) => {
+})
+//http request = get; retrieving data
+app.get('/aboutme',(req, res) => {
   res.sendFile(__dirname + '/aboutme.html')
-}
+})
 
-
-app.listen(3000, () => console.log('LIVE on 3000'))
+app.get('/pictures',(req, res) => {
+  res.sendFile(__dirname + '/pictures.html')
+})
+app.get('/contact',(req, res) => {
+  res.sendFile(__dirname + '/contact.html')
+})
+//post = inserts something
+app.post('/contact', (req, res) => {
+  let {email, text} = req.body;
+  console. log (req. body)
+  return CONTACTS.add(email, text)
+  .then (contact => res.sendFile(__dirname + '/index.html'))
+  res. sendFile(__dirname + '/index.html');
+})
+//listen for requests incoming for the server
+app.listen(PORT, () => console.log(`Your Application is LIVE on localhost:${PORT}`))
